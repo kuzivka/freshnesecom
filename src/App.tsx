800eaf765a264a4fc9a90162eb0a54b62eaf9691
Anhelina Kuziv <anhelinakuziv@gmail.com>
2022-10-04 13:45:30 +0300

    Switch to Hash Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import AllProductsPage from '@pages/AllProductsPage';
 import ProductPage from '@pages/ProductPage';
 import ShoppingCart from '@pages/ShoppingCart';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path="/freshnesecom" element={<AllProductsPage />} />
+        <Route path="/" element={<AllProductsPage />} />
         <Route path="/product-page" element={<ProductPage />} />
         <Route path="/shopping-cart" element={<ShoppingCart />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
