e608bb9f61d16b7bf4af43c32d737a6379659d21
Anhelina Kuziv <anhelinakuziv@gmail.com>
2022-10-14 10:39:47 +0300

    Syntax fix

--- src/store.ts
+++ src/store.ts
@@ -11,6 +11,8 @@ const rootReducer = combineReducers({
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(ecommerceApi.middleware),
+  middleware: (getDefaultMiddleware) => [
+    ...getDefaultMiddleware(),
+    ecommerceApi.middleware,
+  ],
 });
