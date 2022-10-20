9d030de02633d8d3476814d6676af13e8af6e4ce
Anhelina Kuziv <anhelinakuziv@gmail.com>
2022-10-20 15:07:47 +0300

    Add missing line

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,26 +1,26 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme } from '@mui/material/styles';
 
 export const theme = createTheme({
-    palette: {
-      primary: {
-        main: '#6A983C',
-      },
+  palette: {
+    primary: {
+      main: '#6A983C',
     },
-    typography: {
-      fontFamily: ['Poppins', 'sans-serif'].join(','),
-      allVariants: {
-        color: '#151515',
-      },
+  },
+  typography: {
+    fontFamily: ['Poppins', 'sans-serif'].join(','),
+    allVariants: {
+      color: '#151515',
     },
-    components: {
-      MuiLink: {
-        styleOverrides: {
-          root: {
-            fontFamily: 'Open Sans',
-            textDecorationLine: 'none',
-            color: '#6A983C',
-          },
+  },
+  components: {
+    MuiLink: {
+      styleOverrides: {
+        root: {
+          fontFamily: 'Open Sans',
+          textDecorationLine: 'none',
+          color: '#6A983C',
         },
       },
     },
-  });
+  },
+});
